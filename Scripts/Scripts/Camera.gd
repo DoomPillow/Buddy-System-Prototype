dd10302d5a560e16a5583a73409b6d946c0f7549
DoomPillow <90370987+DoomPillow@users.noreply.github.com>
2023-03-10 10:29:38 -0800

    Delete Scripts/Scripts directory

--- Scripts/Scripts/Camera.gd
+++
@@ -1,9 +0,0 @@
-extends Camera2D
-
-var origin = position;
-
-func _process(delta):
-	
-	position = lerp(origin,lerp(Global.playerpos_1,Global.playerpos_2,0.5),0.1);
-	
-	
--- Scripts/Scripts/Global.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var playerpos_1 = Vector2();
-var playerpos_2 = Vector2();
-
-var currentlevel : int = 0;
