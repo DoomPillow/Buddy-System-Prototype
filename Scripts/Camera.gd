073a8d5faca7ebc47825bc85aa29a76742302517
DoomPillow <90370987+DoomPillow@users.noreply.github.com>
2023-03-14 08:11:20 -0700

    Add files via upload

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,9 +1,19 @@
 extends Camera2D
 
 var origin = position;
+onready var greyscale = $'CanvasLayer/ColorRect'
 
 func _process(delta):
 	
-	position = lerp(origin,lerp(Global.playerpos_1,Global.playerpos_2,0.5),0.1);
+	# Easy Pause var
+	var _paused = get_tree().paused;
 	
+	# Move Camera
+	if !_paused:
+		position = lerp(origin,lerp(Global.playerpos_1,Global.playerpos_2,0.25),0.05);
 	
+	# Check for pause toggling
+	if Input.is_action_just_pressed("ui_cancel"):
+		
+		get_tree().paused = !get_tree().paused
+		greyscale.visible = !greyscale.visible
